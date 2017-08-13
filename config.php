29ee5145a03aa1d9813f7a1dbb84426252438677
root <root@Mercury.Helium>
2017-08-13 06:05:42 -0500

    modified:   config.php

--- config.php
+++ config.php
@@ -2,9 +2,9 @@
 //Set this to the token/bot ID of your bot, ex: 1252346142346243 or whatever your code is
 $token = "yourtoken";
 //Set this to your Weather Underground API token, ex: 42362362362 or whatever your code is
-$wutoken = "yourwutoken";
+//$wutoken = "yourwutoken";
 //Wunderground location string in format "STATEABBREVIATION/CITY_NAME", ex: TX/College_Station
-$wulocation = "youtwulocation";
+//$wulocation = "youtwulocation";
 //Set this to 1 to make a chat log under specified dir
 $log = 1;
 //Name for log file
